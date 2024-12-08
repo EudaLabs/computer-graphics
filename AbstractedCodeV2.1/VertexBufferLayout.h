f062866d9df51ac3bdcdd82fe6a1cdcfa1b3224c
MuffinMufido <muffinmufido@gmail.com>
2024-12-09 00:39:45 +0300

    worked around of bug "Explicit specialization in non-namespace" for g++ compier

---
+++ AbstractedCodeV2.1/VertexBufferLayout.h
@@ -0,0 +1,43 @@
+#include "Renderer.h"
+#include <iterator>
+#include <vector>
+#include <GLFW/glfw3.h>
+#include "Renderer.h"
+struct VertexBufferElement
+{
+  unsigned int type;
+  unsigned int count;
+  unsigned char normalized;
+
+  static unsigned int GetSizeOfType(unsigned int type)
+  {
+    switch (type) {
+      case GL_FLOAT: return 4;
+      case GL_UNSIGNED_INT: return 4;
+      case GL_UNSIGNED_BYTE: return 1; 
+    }
+    ASSERT(false);
+    return 0;
+  } 
+};
+
+
+class VertexBufferLayout
+{
+  private:
+    std::vector<VertexBufferElement> m_Elements;
+    unsigned int m_Stride;
+  public:
+    VertexBufferLayout()
+      : m_Stride(0) {}  
+  
+    void Push(unsigned int count)
+    {
+      m_Elements.push_back({ GL_FLOAT, count, GL_FALSE});
+      m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * count;
+    }
+
+    inline const std::vector<VertexBufferElement> GetElements() const {return m_Elements;}
+    inline unsigned int GetStride() const {return  m_Stride;}
+};
+
